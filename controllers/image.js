ddaf7542e34d557a9b0fb5068fcdded4180c0420
Makszim Pecsko <max@recruitd.com>
2020-01-10 15:04:17 +0000

    dockerised api/db

--- controllers/image.js
+++ controllers/image.js
@@ -1,30 +1,29 @@
 const Clarifai = require('clarifai');
 
-const app = new Clarifai.App({   
-    apiKey: '0fd2c5196ec746c696d52ed7524229f3'  
+const app = new Clarifai.App({
+  apiKey: '0fd2c5196ec746c696d52ed7524229f3'
 });
 
-const handleApiCall = (req, res) => {
-    app.models.initModel({
-        id: Clarifai.FACE_DETECT_MODEL,
-        version: "28b2ff6148684aa2b18a34cd004b4fac"
-    })
-    .then(generalModel => generalModel.predict(req.body.input))
+const handleApiCall = () => (req, res) => {
+  app.models
+    .predict('a403429f2ddf4b49b307e318f00e528b', req.body.input)
     .then(response => res.json(response))
     .catch(err => res.status(400).json(err));
-}
+};
 
 const handleImage = db => (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            res.json(entries[0]);
-        }).catch(err => res.status(400).json('Looks like we\'ve got an error.'));
-}
+  const { id } = req.body;
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("Looks like we've got an error."));
+};
 
 module.exports = {
-    handleImage,
-    handleApiCall
+  handleImage,
+  handleApiCall
 };
--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 // IMPORTS
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const knex = require('knex');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const knex = require("knex");
+const morgan = require("morgan");
 
-const signIn = require('./controllers/signIn');
-const register = require('./controllers/register');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const { handleSignIn } = require("./controllers/signIn");
+const { handleRegister } = require("./controllers/register");
+const { handleProfile } = require("./controllers/profile");
+const { handleImage, handleApiCall } = require("./controllers/image");
+
+const port = process.env.PORT || 3000;
 
 // CONNECTING TO DATABASE
 const db = knex({
-    client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: '',
-        database: 'recognition-app-db'
-    }
+  client: "pg",
+  connection: process.env.POSTGRES_URI
 });
-
-db.select('*').from('users');
 
 // EXPRESS INIT
 const app = express();
@@ -29,15 +25,15 @@ const app = express();
 // APPLYING MIDDLEWARE
 app.use(cors());
 app.use(bodyParser.json());
+app.use(morgan("combined"));
 
-app.get('/', (req, res) => {res.send('It works!')});
-app.post('/signIn', signIn.handleSignIn(db, bcrypt));  
-app.post('/register', register.handleRegister(db, bcrypt));
-app.get('/profile/:id', profile.handleProfile(db));
-app.put('/image', image.handleImage(db));
-app.post('/imageUrl', (req, res) => {image.handleApiCall(req, res)});
+app.get("/", (req, res) => res.send("It works!"));
+app.post("/signIn", handleSignIn(db, bcrypt));
+app.post("/register", handleRegister(db, bcrypt));
+app.get("/profile/:id", handleProfile(db));
+app.put("/image", handleImage(db));
+app.post("/imageUrl", handleApiCall(req, res));
+app.get("/history/:user_id");
 
 // LISTENING TO PORT
-app.listen(process.env.PORT, () => {
-    console.log(`App is running on port ${process.env.PORT}`);
-});
+app.listen(port);
